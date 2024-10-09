019a3703f77617d6e8306c6ec7a54d5a89e52e8f
riru4293 <41512570+riru4293@users.noreply.github.com>
2024-10-09 22:25:40 +0900

    Added UART reception

--- main.c
+++ main.c
@@ -1,18 +1,31 @@
 #include <pico/stdlib.h>
+#include <hardware/uart.h>
+#include <hardware/irq.h>
 #include <FreeRTOS.h>
 #include <task.h>
 
-#define GP_LED                      ( 25 )
+#define GP_LED (25U)
+#define UART_TX_PIN (0U)
+#define UART_RX_PIN (1U)
+#define UART_ID uart0
+#define BAUD_RATE 115200
+#define DATA_BITS 8
+#define STOP_BITS 1
+#define PARITY UART_PARITY_NONE
 
-static void task(void *nouse);
+static void blinkTask(void *nouse);
+static void uartTask(void *nouse);
+static void onUartRx(void);
 
 static const uint16_t C_TASK_INTERVAL = 500U;
-static TaskHandle_t gTask = NULL;
+static TaskHandle_t gBlinkTask = NULL;
+static TaskHandle_t gUartTask = NULL;
 static bool gLedOn = true;
 
 int main(void)
 {
-    BaseType_t result = xTaskCreate(task, "Blink", 1024, NULL, 2, &gTask);
+    (void)xTaskCreate(blinkTask, "Blink", 1024, NULL, 2, &gBlinkTask);
+    (void)xTaskCreate(uartTask, "Uart", 1024, NULL, 1, &gUartTask);
     vTaskStartScheduler();
 
     while (1)
@@ -21,18 +34,54 @@ int main(void)
     }
 }
 
-static void task(void *nouse)
+static void blinkTask(void *nouse)
 {
-    gpio_init( GP_LED );
-    gpio_set_dir( GP_LED, GPIO_OUT );
+    gpio_init(GP_LED);
+    gpio_set_dir(GP_LED, GPIO_OUT);
 
     while (1)
     {
-        gpio_put( GP_LED, gLedOn );
+        gpio_put(GP_LED, gLedOn);
         vTaskSuspendAll();
         vTaskDelay(C_TASK_INTERVAL);
         (void)xTaskResumeAll();
         vTaskDelay(C_TASK_INTERVAL);
         gLedOn = !gLedOn;
+    }
+}
+
+static void uartTask(void *nouse)
+{
+    (void)uart_init(UART_ID, BAUD_RATE);
+    gpio_set_function(UART_TX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_TX_PIN));
+    gpio_set_function(UART_RX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_RX_PIN));
+    uart_set_format(UART_ID, DATA_BITS, STOP_BITS, PARITY);
+    uart_set_fifo_enabled(UART_ID, false);
+    uart_set_hw_flow(UART_ID, false, false);
+    int UART_IRQ = UART_ID == uart0 ? UART0_IRQ : UART1_IRQ;
+    irq_set_exclusive_handler(UART_IRQ, onUartRx);
+    irq_set_enabled(UART_IRQ, true);
+    uart_set_irq_enables(UART_ID, true, false);
+
+    uart_puts(UART_ID, "\nHello, uart interrupts\n");
+
+    while (1)
+    {
+        tight_loop_contents();
+    }
+}
+
+static void onUartRx(void)
+{
+    uart_hw_t *uart = uart_get_hw(UART_ID);
+
+    while (uart_is_readable(UART_ID))
+    {
+        uint8_t ch = uart_getc(UART_ID);
+
+        if (uart_is_writable(UART_ID))
+        {
+            uart_putc(UART_ID, ch);
+        }
     }
 }
